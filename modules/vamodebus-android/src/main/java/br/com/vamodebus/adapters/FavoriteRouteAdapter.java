a4085d768295477495ec79d189720c2a30d6586f
edusr <edus.silva.rosa@gmail.com>
2013-07-27 13:07:06 -0300

    improved favorite router adapter

--- modules/vamodebus-android/src/main/java/br/com/vamodebus/adapters/FavoriteRouteAdapter.java
+++ modules/vamodebus-android/src/main/java/br/com/vamodebus/adapters/FavoriteRouteAdapter.java
@@ -1,7 +1,58 @@
 package br.com.vamodebus.adapters;
 
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+import br.com.vamodebus.R;
+import br.com.vamodebus.model.FavoriteRoute;
+import br.com.vamodebus.model.History;
+
 /**
  * Created by edusr on 7/6/13.
  */
-public class FavoriteRouteAdapter {
+public class FavoriteRouteAdapter  extends BaseAdapter {
+
+    Context ctx;
+    List<FavoriteRouteAdapter> favoriteRoutes;
+
+    public FavoriteRouteAdapter(Context ctx, List<FavoriteRouteAdapter> favoriteRoutes){
+        this.ctx = ctx;
+        this.favoriteRoutes = favoriteRoutes;
+    }
+
+    @Override
+    public int getCount() {
+        return favoriteRoutes.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return favoriteRoutes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) this.ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View v = inflater.inflate(R.layout.row_favorite_route, null);
+
+        TextView favoriteRouteDescription = (TextView) v.findViewById(R.id.nameFavoriteRoute);
+        TextView favoriteRouteAccessNumber = (TextView) v.findViewById(R.id.numberAccess);
+        FavoriteRoute favoriteRoute = (FavoriteRoute)getItem(position);
+        favoriteRouteDescription.setText(favoriteRoute.getName());
+        favoriteRouteAccessNumber.setText(favoriteRoute.getAccesNumber());
+        v.setTag(favoriteRoute.getId());
+
+        return v;
+    }
+
 }
